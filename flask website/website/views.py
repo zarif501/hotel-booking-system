2a8b960c36c9acb382d7627b56e101235318f15f
Asa <asa@Mac.TCCB.GOV.TR>
2026-02-22 16:46:04 +0300

    add room vs

--- flask website/website/views.py
+++ flask website/website/views.py
@@ -1,7 +1,30 @@
-from flask import Blueprint
+@views.route('/add_room', methods=['GET', 'POST'])
+def add_room():
+    if request.method == 'POST':
+        name = request.form.get('room_name')
+        price = request.form.get('price')
 
-views = Blueprint('views', __name__)
+        # save to database (later)
 
-@views.route('/')
-def home():
-    return "<h1>It's a hotel booking system project!</h1>"
+        return redirect('/rooms')
+
+    return render_template('add_room.html')
+
+@views.route('/rooms')
+def rooms():
+    rooms = []  # later from database
+    return render_template('rooms.html', rooms=rooms)
+
+@views.route('/edit_room/<int:id>', methods=['GET', 'POST'])
+def edit_room(id):
+    if request.method == 'POST':
+        # update database
+        return redirect('/rooms')
+
+    room = {}  # get from DB
+    return render_template('edit_room.html', room=room)
+
+@views.route('/bookings')
+def bookings():
+    bookings = []
+    return render_template('bookings.html', bookings=bookings)
